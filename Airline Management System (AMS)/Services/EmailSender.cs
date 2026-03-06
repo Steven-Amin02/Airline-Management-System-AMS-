5c07219e81068b4e886001ff3f8246ce4467ead6
YousabAshraf <168154444+YousabAshraf@users.noreply.github.com>
2026-03-06 21:13:51 +0200

    convert brevo to Resend

--- Airline Management System (AMS)/Services/EmailSender.cs
+++ Airline Management System (AMS)/Services/EmailSender.cs
@@ -1,43 +1,32 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Configuration;
-using System.Net;
+using Resend;
 using System.Net.Mail;
 using System.Threading.Tasks;
 
 public class EmailSender : IEmailSender
 {
     private readonly IConfiguration _configuration;
+    private readonly IResend _resend;
 
     public EmailSender(IConfiguration configuration)
     {
         _configuration = configuration;
+
+        var apiKey = _configuration["Resend:ApiKey"];
+        _resend = ResendClient.Create(apiKey);
     }
 
-    public Task SendEmailAsync(string email, string subject, string htmlMessage)
+    public async Task SendEmailAsync(string email, string subject, string htmlMessage)
     {
-        var smtpHost = _configuration["SmtpSettings:SmtpServer"];
-        var smtpPort = int.Parse(_configuration["SmtpSettings:SmtpPort"]);
-        var smtpUser = _configuration["SmtpSettings:SmtpUsername"];
-        var smtpPass = _configuration["SmtpSettings:SmtpPassword"];
-        var senderEmail = _configuration["SmtpSettings:SenderEmail"];
+        var senderEmail = _configuration["Resend:SenderEmail"];
 
-        var smtp = new SmtpClient(smtpHost)
-        {
-            Port = smtpPort,
-            Credentials = new NetworkCredential(smtpUser, smtpPass),
-            EnableSsl = true, // Brevo SMTP requires SSL/TLS
-        };
+        var message = new EmailMessage();
+        message.From = senderEmail;
+        message.To.Add(email);
+        message.Subject = subject;
+        message.HtmlBody = htmlMessage;
 
-        var mail = new MailMessage
-        {
-            From = new MailAddress(senderEmail, "Airline Management System"),
-            Subject = subject,
-            Body = htmlMessage,
-            IsBodyHtml = true,
-        };
-
-        mail.To.Add(email);
-
-        return smtp.SendMailAsync(mail);
+        await _resend.EmailSendAsync(message);
     }
 }
